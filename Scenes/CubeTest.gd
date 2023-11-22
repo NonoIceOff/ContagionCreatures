bf47477ffbfada3d14c18b0e3ecfdc21e65e100c
MathieuMauger <math13.mauger@gmail.com>
2023-11-22 13:59:19 +0100

    add cam on player

--- Scenes/CubeTest.gd
+++ Scenes/CubeTest.gd
@@ -1,28 +1,68 @@
-extends Sprite2D
-var speed = 75
-var dir = Vector2()
+extends CharacterBody2D
+#@export var SpriteSheets:Array[LPCSpriteSheet]
+#@export var DefaultAnimation:LPCAnimation = LPCAnimation.IDLE_DOWN
+var speed = 7
 const FLOOR_NORMAL = Vector2(0,-1)
+var offset_x = 0
+var offset_y = 64
+var left = [Vector2(80 + offset_x , 16 + offset_y), Vector2(64 + offset_x , 16 + offset_y) ,Vector2(48 + offset_x , 16 + offset_y)]
+var right = [Vector2(80 +  offset_x, 32 +  offset_y), Vector2(64 +  offset_x , 32 + offset_y), Vector2(48 +  offset_x , 32 +  offset_y)]
+var up = [Vector2(80 +  offset_x , 48 +  offset_y),Vector2(64  +  offset_x, 48 +  offset_y), Vector2(48 +  offset_x , 48 +  offset_y)]
+var down = [Vector2(80 +  offset_x , 0 +  offset_y), Vector2(64 +  offset_x , 0 +  offset_y), Vector2(48 +  offset_x , 0 +  offset_y)]
+var i = 0
+var direction = -1
+var idle = [ Vector2(64  +  offset_x, 48 +  offset_y),Vector2(64 +  offset_x , 0 +  offset_y),Vector2(64 +  offset_x , 32 + offset_y),  Vector2(64 + offset_x , 16 + offset_y)]
 
-# Called when the node enters the scene tree for the first time.
+
+
+
+
+
+
+
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_up"): 
+	#if Input.is_action_just_pressed("ui_up"):
+	#	position.y += -speed
+	#	get_node("01-generic").region_rect = Rect2(up[2][1],up[2][1],16,16)
+	if Input.is_action_pressed("ui_up"):
+		i += 1
+		direction = 0
+		if i > 29:
+			i = 1
 		position.y += -speed
+		get_node("01-generic").region_rect = Rect2(up[i/10][0],up[i/10][1],16,16)
 	elif Input.is_action_pressed("ui_down"):
+		i += 1
+		direction = 1
+		if i > 29:
+			i = 1
 		position.y += speed
+		get_node("01-generic").region_rect = Rect2(down[i/10][0],down[i/10][1],16,16)
 	elif Input.is_action_pressed("ui_right"):
+		i += 1
+		direction = 2
+		if i > 29:
+			i = 1
 		position.x += speed
+		get_node("01-generic").region_rect = Rect2(right[i/10][0],right[i/10][1],16,16)
 	elif Input.is_action_pressed("ui_left"):
+		i += 1
+		direction = 3
+		if i > 29:
+			i = 1
 		position.x += -speed
+		get_node("01-generic").region_rect = Rect2(left[i/10][0],left[i/10][1],16,16)
 	else :
-		dir.x = 0
+		get_node("01-generic").region_rect = Rect2(idle[direction][0],idle[direction][1],16,16)
+		
+	
 	
 
 	
-func _input(event):
-	print(event)
+
 	
---
+++ addons/LPCAnimatedSprite/LPCSpriteData.gd
@@ -0,0 +1,13 @@
+class_name LPCAnimationData
+
+var FrameCount:int
+var Name:String
+var Row:int
+var Col:int
+var Loop:bool
+func _init(frameCount:int, name:String, row:int, col:int, loop:bool):
+		FrameCount = frameCount
+		Name = name
+		Row = row
+		Col = col
+		Loop = loop
