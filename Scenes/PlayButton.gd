b60aafd4c9c83cb32467d37bd707a40a1307a043
NonoIceOff <54319211+NonoIceOff@users.noreply.github.com>
2023-11-23 14:18:16 +0100

    modifs

--- Scenes/PlayButton.gd
+++ Scenes/PlayButton.gd
@@ -2,6 +2,11 @@ extends Button
 
 var menu_fade_out = false
 
+@onready var map_scene = preload("res://Scenes/main_map.tscn")
+
+func _ready():
+	get_node("../ProgressBar").visible = false
+
 func _physics_process(delta):
 	if menu_fade_out == true:
 		set_modulate(lerp(get_modulate(), Color(1,1,1,0), 0.02))
@@ -9,9 +14,28 @@ func _physics_process(delta):
 func _process(delta):
 	if float(get_modulate()[3]) <= 0.1:
 		get_tree().change_scene_to_file("res://Scenes/main_map.tscn")
-		print("aaaa")
 
 func _on_pressed():
+	get_node("../ProgressBar").visible = true
 	get_node("../Sounds").stream = load("res://Sounds/coinc.mp3")
 	get_node("../Sounds").playing = true
 	menu_fade_out = true
+	
+func load_scene(current_scene,next_scene):
+	var loading_scene_instance = map_scene.instance()
+	get_tree().get_root().call_deferred("add_child", loading_scene_instance)
+	
+	var loader = ResourceLoader.load_threaded_request(next_scene)
+	if loader == null:
+		print("Une erreur ests urvenue lors du chargement de la scène")
+		return
+	current_scene.queue_free()
+	
+	await get_tree().create_timer(1000).timeout
+	
+	while true:
+		var error = loader.poll()
+		if error == OK:
+			get_node("../ProgressBar").value = float(loader.get_stage()/loader.get_stage_count())
+			
+	
