d69997d7e13d3670d22279230e4b7b1542bea622
NonoIceOff <54319211+NonoIceOff@users.noreply.github.com>
2023-12-20 13:10:57 +0100

    chat

---
+++ Scenes/Chat.gd
@@ -0,0 +1,32 @@
+extends Control
+
+var messages_container
+
+func _ready():
+	messages_container = get_node("ScrollContainer/VBoxContainer")
+
+func _on_send_pressed():
+	send_message(get_node("MessageEdit").text,"pseudodetest")
+	rpc("send_message", get_node("MessageEdit").text, "pseudotest")
+	get_node("MessageEdit").text = ""
+
+@rpc("any_peer")
+func send_message(message,pseudo):
+	var container = Panel.new()
+	container.custom_minimum_size = Vector2(480,32)
+	messages_container.add_child(container)
+	
+	var pseudo_text = Label.new()
+	pseudo_text.custom_minimum_size = Vector2(96,20)
+	pseudo_text.position = Vector2(0,-16)
+	pseudo_text.text = pseudo
+	container.add_child(pseudo_text)
+	
+	var message_text = RichTextLabel.new()
+	message_text.custom_minimum_size = Vector2(472,30)
+	message_text.position = Vector2(8,4)
+	message_text.text = "[color=gray][i]"+message
+	message_text.bbcode_enabled = true
+	container.add_child(message_text)
+	
+	get_node("ScrollContainer").scroll_vertical = 99999999
