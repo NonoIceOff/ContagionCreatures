129063ea96cf7f240673d792f499eef3a892ebce
MathieuMauger <math13.mauger@gmail.com>
2024-02-20 11:56:33 +0100

    Systeme de ping

--- Scenes/Full_screen_map.gd
+++ Scenes/Full_screen_map.gd
@@ -4,9 +4,16 @@ extends Control
 
 #Point jaune dans la miniMap
 var pin
+var points_stock: Array = []
+#var point_player : ColorRect = null
 
 func _ready():
 	pin = get_node("/root/main_map/MobPNJ/Ennemy").position
+	#var point_sprite = ColorRect.new()
+	#point_sprite.color = Color(1,1,0)
+	#point_sprite = Rect2(0,0,16,16)
+	#add_child(point_sprite)
+	
 	
 func _physics_process(delta):
 	get_node("AnimationPlayer").current_animation = "pin"
@@ -17,13 +24,13 @@ func _physics_process(delta):
 	
 	if get_node("PinPoint").position.x < 20:
 		get_node("PinPoint").position.x = 20
-		
+
 	if get_node("PinPoint").position.y < 20:
 		get_node("PinPoint").position.y = 20
-		
+
 	if get_node("PinPoint").position.x > 32+180:
 		get_node("PinPoint").position.x = 32+180
-		
+
 	if get_node("PinPoint").position.y > 32+180:
 		get_node("PinPoint").position.y = 32+180
 		
@@ -32,3 +39,22 @@ func _physics_process(delta):
 func change_pin(position):
 	pin = position
 	
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		var mouse_pos: Vector2 = get_local_mouse_position()
+		var previous_pointReplace = points_stock.size()-1
+		if previous_pointReplace >= 0:
+			points_stock[previous_pointReplace] = mouse_pos
+		else:
+			points_stock.append(mouse_pos)
+		draw_point(mouse_pos)
+		points_stock.append(mouse_pos)
+
+func draw_point(position: Vector2) -> void:
+	var point_sprite = ColorRect.new() 
+	point_sprite.color = Color(1,1,0)
+	point_sprite.size = Vector2i(8.0, 8.0)
+	point_sprite.position = position.round()
+	add_child(point_sprite)
+
