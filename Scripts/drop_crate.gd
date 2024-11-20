b32d857a8d84c895e77e7f52aa10fb08acc9f31f
Mathieu M <math13.mauger@gmail.com>
2024-11-20 23:53:32 +0100

    scène drop items after fight

--- Scripts/drop_crate.gd
+++ Scripts/drop_crate.gd
@@ -1,115 +1,159 @@
 extends Node
 
 const API_URL = "https://contagioncreaturesapi.vercel.app/api"
-const CREATURES_FILE_PATH = "res://Constantes/creatures.json"
-@onready var drop_rate_second_item = 5
 @onready var common_item_texture = $Items/CommonItem
 @onready var rare_item_texture = $Items/RareItem
+@onready var common_item_label = $Items/CommonItemLabel
+@onready var rare_item_label = $Items/RareItemLabel
 @onready var crate_locked_texture = $Crates/CommonCrate
 @onready var crate_gold_locked_texture = $Crates/RareCrate
 
 var crate_opened = false
-var has_second_item = false
-var creatures_data = []
 var items_data = []
-var http_get_creatures = HTTPRequest.new()
+var current_item_index = 0
+var items_revealed = false
 var http_get_creature_drops = HTTPRequest.new()
 
 func _ready():
-	has_second_item = (randf() * 100) < drop_rate_second_item
-	crate_locked_texture.visible = !has_second_item
-	crate_gold_locked_texture.visible = has_second_item
-	
-	add_child(http_get_creatures)
-	add_child(http_get_creature_drops)  
-	
-	var creature_id = randi_range(1, 10)
-	_fetch_creature(creature_id)
-	_fetch_creature_drops(creature_id)
-	creatures_data = _load_local_json()
+	common_item_texture.visible = false
+	rare_item_texture.visible = false
+	common_item_label.visible = false
+	rare_item_label.visible = false
+
+	add_child(http_get_creature_drops)
+	http_get_creature_drops.connect("request_completed", Callable(self, "_on_creature_drops_received"))
+	http_get_creature_drops.request(API_URL + "/creatures/1/drops")
+
 
 func _input(event):
-	if crate_opened:
-		return  
-	
-	if event.is_action_pressed("Space") or event.is_action_pressed("leftClick"):
+	if crate_opened and (event.is_action_pressed("Space") or event.is_action_pressed("leftClick")):
+		if !items_revealed:
+			show_next_item()
+		else:
+			reset_items_or_exit()
+
+	elif not crate_opened and (event.is_action_pressed("Space") or event.is_action_pressed("leftClick")):
 		if crate_locked_texture.visible and !crate_gold_locked_texture.visible:
 			crate_locked_texture.play("BaseOpenCase")
-			await get_tree().create_timer(1).timeout
+			await get_tree().create_timer(1).timeout  
 			crate_opened = true
 			open_crate_effect()
 		elif !crate_locked_texture.visible and crate_gold_locked_texture.visible:
-			crate_gold_locked_texture.play("RareOpenCase")
-			await get_tree().create_timer(1).timeout
+			crate_gold_locked_texture.play("RareOpenCase")  
+			await get_tree().create_timer(1).timeout 
 			crate_opened = true
 			open_crate_effect()
 
+
 func open_crate_effect():
-	crate_locked_texture.scale = Vector2(4, 4) 
-	crate_locked_texture.modulate.a = 0.3 
+	await get_tree().create_timer(0.5).timeout 
+	crate_locked_texture.scale = Vector2(4, 4)
+	crate_locked_texture.modulate.a = 0.3
+	crate_locked_texture.visible = false
 	crate_gold_locked_texture.scale = Vector2(4, 4)
 	crate_gold_locked_texture.modulate.a = 0.3
-	
+	crate_gold_locked_texture.visible = false
 	show_first_item()
 
+
 func show_first_item():
-	if common_item_texture:
+	if items_data.size() > 0:
 		common_item_texture.visible = true
-	
-	if has_second_item:
-		show_second_item()
+		common_item_texture.position = Vector2(960 - (common_item_texture.texture.get_width() * 2) / 2, 540)  
+		common_item_texture.scale = Vector2(2, 2)  
+		
+		common_item_label.visible = true
+		common_item_label.text = items_data[0].get("name", "Item")  
+		print(items_data[0].get("name", "Item"))
+		
+		var label_width = common_item_label.get_minimum_size().x
+		common_item_label.position = Vector2(
+			common_item_texture.position.x + (common_item_texture.texture.get_width() * 2 - label_width) / 2,
+			common_item_texture.position.y + common_item_texture.texture.get_height() * 2 + 10
+		)
+
+		current_item_index = 0
+
+	if items_data.size() <= 1:
+		items_revealed = true
+
 
 func show_second_item():
-	if rare_item_texture:
+	if items_data.size() > 1:
 		rare_item_texture.visible = true
-
-func _fetch_creature(creature_id):
-	http_get_creatures.request(API_URL + "/creatures/" + str(creature_id))
-
-func _fetch_creature_drops(creature_id):
-	http_get_creature_drops.request(API_URL + "/creatures/" + str(creature_id) + "/drops")
-	http_get_creature_drops.connect("request_completed", Callable(self, "_on_http_request_completed"))
-
-func _on_http_request_completed(result, response_code, headers, body):
-	if response_code == 200:
-		var data = JSON.parse_string(body.get_string_from_utf8())
-		if data == null:
-			print("Erreur dans la réponse de l'API.")
-			return
+		rare_item_texture.position = Vector2(960 - (rare_item_texture.texture.get_width() * 2) / 2, 540)  
+		rare_item_texture.scale = Vector2(2, 2) 
+		rare_item_label.visible = true
+		rare_item_label.text = items_data[1].get("name", "Item")
+		print(items_data[1].get("name", "Item"))
 		
-		if "creature" in data:
-			creatures_data = data["creature"]
-			print("Données de créature chargées :", creatures_data)
-		elif "drops" in data:
-			items_data = data["drops"]
-			print("Données des items (drops) chargées :", items_data)
-			apply_item_textures() 
+		var label_width = rare_item_label.get_minimum_size().x
+		rare_item_label.position = Vector2(
+			rare_item_texture.position.x + (rare_item_texture.texture.get_width() * 2 - label_width) / 2,
+			rare_item_texture.position.y + rare_item_texture.texture.get_height() * 2 + 10
+		)
+
+		current_item_index = 1
+
+func show_next_item():
+	if current_item_index == 0 and items_data.size() > 1:
+		common_item_texture.visible = false
+		common_item_label.visible = false
+		show_second_item()
+	else:
+		common_item_texture.visible = true
+		rare_item_texture.visible = items_data.size() > 1
+
+		common_item_texture.position = Vector2(750 - (common_item_texture.texture.get_width() * 2) / 2, 540)
+		rare_item_texture.position = Vector2(1170 - (rare_item_texture.texture.get_width() * 2) / 2, 540)
+
+		common_item_label.position = common_item_texture.position + Vector2(common_item_texture.texture.get_width(), 60)
+		rare_item_label.position = rare_item_texture.position + Vector2(rare_item_texture.texture.get_width(), 60)
+
+		common_item_label.visible = true
+		rare_item_label.visible = items_data.size() > 1
+
+		items_revealed = true
+
+
+func reset_items_or_exit():
+	if items_revealed:
+		queue_free()  
+
+
+func _on_creature_drops_received(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	print("Result Code:", result)
+	print("HTTP Response Code:", response_code)
+	var response_text = body.get_string_from_utf8()
+	print("Response Body:", response_text)
+
+	if response_code == 200:
+		var parse_result = JSON.parse_string(response_text)
+		if typeof(parse_result) == TYPE_ARRAY:
+			items_data = parse_result
+			print("Items Data Received:", items_data)
+
+			if items_data.size() > 1:
+				crate_locked_texture.visible = false
+				crate_gold_locked_texture.visible = true
+			else:
+				crate_locked_texture.visible = true
+				crate_gold_locked_texture.visible = false
+
+			apply_item_textures()
+	else:
+		print("Failed to fetch creature drops. HTTP code:", response_code)
 
 
 func apply_item_textures():
-	#for item in items_data:
-		#var texture_path = "res://Textures/" + item["texture"]  
-		#var texture = load(texture_path)
-		
-		if items_data.size() > 0:
-			var first_item_texture_path = "res://Textures/" + items_data[0]["texture"]
-			var first_item_texture = load(first_item_texture_path)
-			if first_item_texture and common_item_texture:
-				common_item_texture.texture = first_item_texture
-				common_item_texture.visible = true
-		
-		if has_second_item and items_data.size() > 1:
-			var second_item_texture_path = "res://Textures/" + items_data[1]["texture"]
-			var second_item_texture = load(second_item_texture_path)
-			if second_item_texture and rare_item_texture:
-				rare_item_texture.texture = second_item_texture
-				rare_item_texture.visible = true
+	if items_data.size() > 0:
+		var first_item_texture_path = "res://Textures/" + items_data[0].get("texture", "default_texture.png")
+		var first_item_texture = load(first_item_texture_path)
+		if first_item_texture:
+			common_item_texture.texture = first_item_texture
 
-func _load_local_json():
-	var json_as_text = FileAccess.get_file_as_string(CREATURES_FILE_PATH)
-	var parse_result = JSON.parse_string(json_as_text)
-	if parse_result == null:
-		print("Erreur lors du chargement du fichier JSON local.")
-		return []
-	print("Type de données JSON correct pour le fichier local.")
-	return parse_result
+	if items_data.size() > 1:
+		var second_item_texture_path = "res://Textures/" + items_data[1].get("texture", "default_texture.png")
+		var second_item_texture = load(second_item_texture_path)
+		if second_item_texture:
+			rare_item_texture.texture = second_item_texture
