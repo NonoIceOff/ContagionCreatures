1fa982f47f732f665e649a5c810945f1de9920a3
NonoIceOff <nonocraft28@gmail.com>
2024-12-11 10:27:26 +0100

    New Minimap

--- Scenes/map_3.gd
+++ Scenes/map_3.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var transition_scene = $ui/Transition/AnimationPlayer
 @onready var soundEffect = $SoundEffectFx
 func _ready() -> void:
+		
 	transition_scene.play("transition_to_screen")
 	await get_tree().create_timer(0.3).timeout
 	soundEffect.play()
