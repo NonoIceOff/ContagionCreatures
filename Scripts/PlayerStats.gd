fead0f03409082ecf26423e9cf219caebf2feb21
KasutoSoei <dimanchegwendal@gmail.com>
2025-03-03 16:57:57 +0100

    base of the new ui

--- Scripts/PlayerStats.gd
+++ Scripts/PlayerStats.gd
@@ -7,9 +7,12 @@ var character = 0
 
 ## Stats
 var health = 100
-var monnaie = 0
+var armor = 100
+var movement_speed = 1
+var money = 0
 var level = 1
 var pseudo = ""
+var xp_points = 0
 
 ## Animal affecté
 var animal_id = 0
