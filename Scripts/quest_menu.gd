06bd5ebc6171a232d9993a05c5b1afbe38e2af17
NonoIceOff <nonocraft28@gmail.com>
2025-04-04 15:48:20 +0200

    Amélioration menu de quêtes

--- Scripts/quest_menu.gd
+++ Scripts/quest_menu.gd
@@ -21,7 +21,7 @@ func _ready():
 		panel.custom_minimum_size = Vector2(832, 128)
 
 		var title = Label.new()
-		title.text = quest.title
+		title.text = str(quest.title).to_upper()
 		title.name = "Title"
 		title.position = Vector2(16, 0)
 		title.add_theme_font_size_override("font_size", 64)
@@ -62,10 +62,55 @@ func _ready():
 func _on_quest_selected(quest_id):
 	if Quests.quests.get(quest_id).finished == false:
 		Quests.set_quest(quest_id)
+		var quest_status_node = get_node("QuestInfos/QuestStatus")
+		var quest = Quests.quests[quest_id]
+		var descriptions = quest["mini_descriptions"]
+		var current_stade = quest["stade"]
+		
 		get_node("AudioStreamPlayer").stream = load("res://Sounds/click.mp3")
 		get_node("AudioStreamPlayer").playing = true
 		get_node("QuestInfos/TitreQuete").text = Quests.quests[quest_id]["title"]
 		get_node("QuestInfos/DescriptionQuete").text = "[color=black]" + tr(Quests.quests[quest_id]["long_description"]) + "[/color]\n\n[color=orange][i][font_size=46]" + tr(Quests.quests[quest_id]["mini_descriptions"][Quests.quests[quest_id]["stade"]]) + "[/font_size][/i][/color]"
+		get_node("QuestInfos/QuestStatus").text = ""
+		
+		get_node("QuestInfos/ProgressBar").max_value = descriptions.size()
+		get_node("QuestInfos/ProgressBar").value = Quests.quests[quest_id]["stade"]
+		
+		
+		
+		
+		
+
+		quest_status_node.text = ""  # On vide le texte avant d'ajouter les nouvelles lignes
+
+		for i in descriptions.size():
+			# Ajout d'un saut de ligne
+			quest_status_node.text += "\n"
+
+			if i < current_stade:
+				quest_status_node.text += "[color=gray]   ✔️   "  # Étape passée (grisé)
+				quest_status_node.text += str(descriptions[i])
+			elif i == current_stade:
+				quest_status_node.text += "[color=lime]   🚧   "  # Étape actuelle (vert)
+				quest_status_node.text += str(descriptions[i])
+			else:
+				quest_status_node.text += "[color=black]   ❌   "  # Étape future (normal)
+				quest_status_node.text += masquer_texte(str(descriptions[i]))
+
+func masquer_texte(original_text: String) -> String:
+	var length = original_text.length()
+	var half_length = length / 2  # On masque la moitié du texte
+	
+	var obscured_text = ""
+	for i in length:
+		if i < half_length:
+			if original_text[i] == " ":
+				obscured_text += " "  # Conserve les espaces
+			else:
+				obscured_text += "▒"  # Masque avec des blocs
+
+	return obscured_text
+
 
 func open():
 	visible = true
