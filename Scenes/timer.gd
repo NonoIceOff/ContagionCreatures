1df538387c704e752d83c4466817b74339c7228c
NonoIceOff <nonocraft28@gmail.com>
2025-04-08 11:19:30 +0200

    First part of intro

--- Scenes/timer.gd
+++ Scenes/timer.gd
@@ -1,18 +1,19 @@
 extends RichTextLabel
 
-var time_left : float = 60.0  # Temps restant en secondes
+var time_left : float = 60.0
 
 func _process(delta: float) -> void:
-	time_left -= delta
-	var minutes = int(time_left) / 60
-	var seconds = int(time_left) % 60
+	if visible == true:
+		time_left -= delta
+		var minutes = int(time_left) / 60
+		var seconds = int(time_left) % 60
 
-	# Formatage du temps
-	var time_text = "%02d:%02d" % [minutes, seconds]
+		# Formatage du temps
+		var time_text = "%02d:%02d" % [minutes, seconds]
 
-	# Affichage avec effet de shake
-	text = "[shake rate="+str(seconds)+" level=30 connected=1]" + time_text
+		# Affichage avec effet de shake
+		text = "[shake rate="+str(seconds)+" level=30 connected=1]" + time_text
 
-	# Arrêter le compte à rebours à 00:00
-	if time_left <= 0:
-		time_left = 0
+		# Arrêter le compte à rebours à 00:00
+		if time_left <= 0:
+			time_left = 0
